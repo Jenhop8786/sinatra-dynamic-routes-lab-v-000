04dcde18bda57673930e277b1885719981de7e2b
Jenelynrose Hopkins <jenelyn.hopkins@gmail.com>
2018-02-12 03:30:44 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -2,42 +2,42 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
- get '/reversename/:name' do 
-   params[:name].reverse 
+ get '/reversename/:name' do
+   params[:name].reverse
 end
 
- get '/square/:number' do 
+ get '/square/:number' do
    num = params[:number]
    (num.to_i**2).to_s
- end 
+ end
 
- get '/say/:number/:phrase' do 
+ get '/say/:number/:phrase' do
    final_string = ""
-   (params[:number].to_i).times do 
+   (params[:number].to_i).times do
      final_string += "#{params[:phrase]}\n"
-   end 
+   end
    final_string
- end 
+ end
 
- get '/say/:word1/:word2/:word3/:word4/:word5' do 
+ get '/say/:word1/:word2/:word3/:word4/:word5' do
    "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
- end 
+ end
 
- get '/:operation/:number1/:number2' do 
+ get '/:operation/:number1/:number2' do
    operation = params[:operation]
    number1 = params[:number1].to_i
    number2 = params[:number2].to_i
    if operation == "add"
      answer = number1 + number2
    elsif operation == "subtract"
-   answer = number1 - number2 
+   answer = number1 - number2
  elsif operation == "multiply"
-   answer = number1 * number2 
+   answer = number1 * number2
  elsif operation == "divide"
-   answer = number1 / number2 
- else 
+   answer = number1 / number2
+ else
    answer = "Unable to perform this operation"
- end 
- answer.to_s 
- end 
+ end
+ answer.to_s
+ end
  end 
